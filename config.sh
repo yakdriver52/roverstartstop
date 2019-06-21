a06848d5970bdd42e6ad36d2efec0f53a4a6e2e0
Richard Law <rnlaw23@gmail.com>
2019-06-21 15:07:51 -0400

    Removed video starting code from gcstart and put it in videostart. Added an ENCODER configuration option [axis|nvidia] in config.sh. Videostart uses above option to determine pipeline.

--- config.sh
+++ config.sh
@@ -10,6 +10,9 @@ ENCODER_IP=192.168.1.45
 GSTD_CLIENT="gstd-client -q -a $SERVER_IP"
 # Change this to zero when using an actual vehicle
 SIMULATED=1
+# Only one of these should be uncommented
+ENCODER=axis
+# ENCODER=nvidia
 
 red=`tput -Txterm-256color setaf 1`
 green=`tput -Txterm-256color setaf 2`
